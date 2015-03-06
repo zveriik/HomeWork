4956b5b448e2e3655f93939cd83e5788255ceced
Zveriki <zverik@gmail.com>
2015-03-06 11:49:34 +0300

    idea reformat code

--- src/java/homeWorks/HomeWork1.java
+++ src/java/homeWorks/HomeWork1.java
@@ -4,32 +4,36 @@ import java.util.Scanner;
 
 public class HomeWork1 {
 
+    static int count = 0;
+
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
 
         System.out.println("Загадайте число от 1 до 100");
+        count = 0;
         int a = findNumberWithoutFor(in, initArray(), 0, 99);
-        System.out.print("Ваше число: " + a);
+        System.out.print("Ваше число: " + a + ", попыток: " + count);
     }
 
-    private static int[] initArray(){
+    private static int[] initArray() {
         int[] initArray = new int[100];
-        for (int i = 0; i<initArray.length; i++){
+        for (int i = 0; i < initArray.length; i++) {
             initArray[i] = i;
         }
         return initArray;
     }
 
-    private static int findNumberWithoutFor(Scanner in, int[] array, int first, int last){
-        int pivot = first + (last - first)/2;
-        int check = array[pivot]+1;
+    private static int findNumberWithoutFor(Scanner in, int[] array, int first, int last) {
+        int pivot = first + (last - first) / 2;
+        int check = array[pivot] + 1;
         System.out.println(check);
         System.out.println("Больше [+], меньше [-], угадал [=]");
         String str = in.nextLine();
-        if (str.equals("=")){
+        count++;
+        if (str.equals("=")) {
             return check;
-        } else if (str.equals("+")){
+        } else if (str.equals("+")) {
             return findNumberWithoutFor(in, array, pivot + 1, last);
         } else {
             return findNumberWithoutFor(in, array, first, pivot);
