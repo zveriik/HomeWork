b4452a44143b454d5cfd211fe883ffaa50457e3a
Zveriki <zverik@gmail.com>
2015-03-13 19:46:47 +0300

    experiments with sorting word lenght

--- src/java/homeWorks/LSDSorting.java
+++ src/java/homeWorks/LSDSorting.java
@@ -11,22 +11,17 @@ public class LSDSorting {
     public static void main(String[] args) {
 
         int[] array = generateRandomArray(1000000);
-        int[] copy = new int[array.length];
-        System.arraycopy(array, 0, copy, 0, array.length);
+        int[] copy1 = array.clone();
+        int[] copy2 = array.clone();
 
         System.out.println(Arrays.toString(array));
         long start = System.currentTimeMillis();
-        sortAsString(array);
+        sortLSD(copy1);
         System.out.println("LSD time: " + (System.currentTimeMillis() - start));
-        start = System.currentTimeMillis();
-        Arrays.sort(copy);
-        System.out.println("QuickSort time: " + (System.currentTimeMillis() - start));
 
-//        int mask = 0b1;
-//
-//        for (int i = 0; i<32; i++) {
-//            System.out.print(mask & 1000000 >>> 31-i);
-//        }
+        start = System.currentTimeMillis();
+        Arrays.sort(copy2);
+        System.out.println("QuickSort time: " + (System.currentTimeMillis() - start));
 
     }
 
@@ -39,57 +34,57 @@ public class LSDSorting {
         return array;
     }
 
-    private static void sort(int[] a) {
+    //iteration int by bit too slow, by byte optimal
+    private static void sortLSD(int[] a) {
 
-        int n = a.length;
-        int r = 2;
-        int[] aux = new int[n];
-        for (int d = 0; d < 20; d++) {
+        int len = a.length;
+        int iteration = 256;
+        int[] aux = new int[len];
+        for (int d = 0; d < 4; d++) {
 
-            int[] count = new int[r + 1];
+            int[] count = new int[iteration + 1];
 
-            for (int i = 0; i < n; i++) {
-                count[((0b1 & a[i] >>> d) + 1)]++;
+            for (int i = 0; i < len; i++) {
+                count[((0b11111111 & a[i] >>> d) + 1)]++;
             }
 
             for (int i = 0; i < i; i++) {
                 count[i + 1] += count[i];
             }
 
-            for (int i = 0; i < n; i++) {
-                aux[count[(0b1 & a[i] >>> d)]++] = a[i];
+            for (int i = 0; i < len; i++) {
+                aux[count[(0b11111111 & a[i] >>> d)]++] = a[i];
             }
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < len; i++) {
                 a[i] = aux[i];
             }
         }
     }
 
+    //some experiment -- too slow
     public static void sortAsString(int[] a) {
 
-        int N = a.length;
-        int R = 10;
-        int[] aux = new int[N];
+        int len = a.length;
+        int iteration = 10;
+        int[] aux = new int[len];
         for (int d = 1; d >= 0; d--) {
 
-            int[] count = new int[R + 1];     // Compute frequency counts.
+            int[] count = new int[iteration + 1];
 
-            for (int i = 0; i < N; i++) {
+            for (int i = 0; i < len; i++) {
                 String number = Integer.toString(a[i]);
                 number = "000000".substring(0, 6 - number.length()) + number;
-//                System.out.println(number);
                 count[Character.getNumericValue(number.charAt(5 - d)) + 1]++;
-
             }
-            for (int r = 0; r < R; r++)     // Transform counts to indices.
+            for (int r = 0; r < iteration; r++)
                 count[r + 1] += count[r];
-            for (int i = 0; i < N; i++) {     // Distribute.
+            for (int i = 0; i < len; i++) {
                 String number = Integer.toString(a[i]);
                 number = "000000".substring(0, 6 - number.length()) + number;
                 aux[count[Character.getNumericValue(number.charAt(5 - d))]++] = a[i];
             }
-            for (int i = 0; i < N; i++)     // Copy back.
+            for (int i = 0; i < len; i++)
                 a[i] = aux[i];
         }
     }
